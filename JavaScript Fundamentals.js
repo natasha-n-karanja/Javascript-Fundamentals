5473a1a02cc7506341e5fb8490d281c11d972786
natashankaranja <natashankaranja@gmail.com>
2025-03-20 07:59:06 +0300

    completed

--- JavaScript Fundamentals.js
+++ JavaScript Fundamentals.js
@@ -1,5 +1,91 @@
 // Variables (let, const, var)
 
+//Declaring
+
+let name = "Jr";
+const age = 1;
+
+//Naming
+
+let userName = "Bobby";
+let $age = 6;
+let _Breed_ = "GermanShepherd";
+
+//Reasignment
+
+let newName = "John";
+newName = "Jane";
+
 // Data types (string, number, boolean, null, undefined, object, array)
+//String
+let message = "Heyy, World!";
+
+//Number
+let num = 42;
+let price = 19.99;
+
+//Boolean
+let isJavaScriptFun = true;
+let isNotJavaScriptFun = false;
+
+//Object
+let person = { name: "Bobby", age: 6 };
+
+//Array
+let fruits = ["Apple", "Banana", "Cherry"];
+
+//Null
+let emptyValue = null;
+
+//Undefined
+let notDefined;
 
 // Operators (+, -, *, /, ===, !==, &&, ||, !)
+
+//Arithmetic
+let addition = 5 + 3; // 8
+let subtaction = 10 - 6; // 4
+let multiplication = 4 * 3; // 12
+let division = 12 / 4; // 3
+let modulus = 10 % 3; // 1  //(remainder)
+let exponentiation = 2 ** 3; // 8
+
+//Comparison
+// ==	- Equal
+// ===	- Strict Equal
+// != - Not Equal	
+// !==	- Strict Not Equal	
+// >	- Greater Than	
+// <	- Less Than	
+// >=	- Greater Than or Equal	
+// <=	- Less Than or Equal
+
+let equal = 5 === 5; // true
+let notEqual = 5 !== 5; // false
+let greaterThan = 5 > 3; // true
+let lessThan = 3 < 5; // true
+let greaterThanOrEqual = 5 >= 5; // true
+let lessThanOrEqual = 5 <= 5; // true
+
+// Logical
+// && - AND
+// ` - backtick
+// ! - NOT
+
+let and = true && true; // true
+let or = true || false; // true
+let not = !true; // false
+
+//Assignment
+// =	 --x = 5	-- x = 5
+// +=	-- x += 3	-- x = x + 3
+// -= --	x -= 2	-- x = x - 2
+// *=	-- x *= 4	--x  = x * 4
+// /=	-- x /= 2	-- x = x / 2
+
+let x = 5;
+x = x + 3; // x = 8
+x += 3; // x = 11
+x -= 2; // x = 9
+x *= 4; // x = 36
+x /= 2; // x = 18
